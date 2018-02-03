69bf17848a18648eb3c8e1e0fc052b19cd4c930b
Erik <erik.dilemma@gmail.com>
2018-02-03 13:50:53 +0100

    Log treatments with microseconds precision.

--- NightscoutUploadKit/TimeFormat.swift
+++ NightscoutUploadKit/TimeFormat.swift
@@ -9,9 +9,21 @@
 import Foundation
 
 class TimeFormat: NSObject {
-    private static var formatterISO8601 = DateFormatter.ISO8601DateFormatter()
+    private static var formatter = DateFormatter()
     
     static func timestampStrFromDate(_ date: Date) -> String {
-        return formatterISO8601.string(from: date)
+        formatter.dateFormat = "ZZZZZ"
+        let tzString = formatter.string(from: date)
+
+        let cal = Calendar.current
+        let comps = cal.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond],
+                               from: date)
+        let microSeconds = lrint(Double(comps.nanosecond!)/1000) // Divide by 1000 and round
+
+        let formatted = String(format: "%04ld-%02ld-%02ldT%02ld:%02ld:%02ld.%06ld",
+                       comps.year!, comps.month!, comps.day!,
+                       comps.hour!, comps.minute!, comps.second!,
+                       microSeconds) + tzString
+        return formatted
     }
 }
